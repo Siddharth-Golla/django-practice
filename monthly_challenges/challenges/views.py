48a316bac98dc42f108afa1c953faff346bc9e24
Siddharth Golla <siddharth.golla87@gmail.com>
2022-11-19 14:57:06 +0530

    added view for each month and attached urls

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+monthly_challenge = {
+    "january":"Read atleast one 300+ pages book",
+    "february":"Walk for atleast 20kms",
+    "march":"Learn any new programming language",
+    "april":"",
+    "may": "",
+    "june": "",
+    "july": "",
+    "august": "",
+    "september": "",
+    "october": "",
+    "november": "",
+    "december": "",
+
+}
 # Create your views here.
+def monthly_challenges(request, month):
+    challenge_text = monthly_challenge[month]
+    return HttpResponse(challenge_text)
