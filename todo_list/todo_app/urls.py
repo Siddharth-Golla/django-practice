02b941a5245587e50c4b284c3bf10aca9f2b8a77
Siddharth Golla <siddharth.golla87@gmail.com>
2022-12-04 21:35:09 +0530

    Basic CRUD views and templates done

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.TaskList.as_view(), name="tasks"),
     path("task/<int:pk>", views.TaskDetail.as_view(), name='task'),
+    path("task-create/", views.TaskCreate.as_view(), name="task-create"),
+    path("task-edit/<int:pk>", views.TaskEdit.as_view(), name="task-edit"),
+    path("task-delete/<int:pk>", views.TaskDelete.as_view(), name="task-delete"),
 ]
--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -12,3 +13,19 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskEdit(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    template_name = 'todo_app/task_delete.html'
