149c71829396ff8df5ab2ac2cbbed741d25544b6
Siddharth Golla <siddharth.golla87@gmail.com>
2022-12-05 13:47:26 +0530

    Added customlogin view and default logoutview

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,9 +1,21 @@
 from django.urls import reverse_lazy
+
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Task
 
+
+class CustomLogin(LoginView):
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
